c6af7c003c896b38fd119ce819caa4fbdd4a3f74
Syeda Sana <sana.shah1740@gmail.com>
2023-08-20 07:37:24 -0700

    some code to get output on API

--- src/main/java/springDemo/Main.java
+++ src/main/java/springDemo/Main.java
@@ -1,7 +1,10 @@
 package springDemo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -20,35 +23,31 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    /*
-     * 
-     * // expressed as endpoints that clients can call--->>
-     * // for this to work, class should have annotation of RestController-->>
-     * // it is path provided of a class to get "Hello" msg---->>
-     * 
-     * @GetMapping("/greet")
-     * public greetResponse greet() {
-     * greetResponse response = new greetResponse("Hello",
-     * List.of("Java", "HTML", "CSS", "JavaScript", "Spring"),
-     * new Person("John", 30, 40000));
-     * return response;
-     * }
-     * 
-     * record Person(String name, int age, double savings) {
-     * }
-     * 
-     * record greetResponse(String greet,
-     * List<String> favProgrammingLanguages, Person person) {
-     * }
-     * // this record line is equivalent to greetResponse class---->>
-     * // class greetResponse {
-     * // private final String greet;
-     * // greetResponse(String greet) {
-     * // this.greet = greet;
-     * // }
-     * // public String getGreet() {
-     * // return greet;
-     * // }
-     * // }
-     */
+    // expressed as endpoints that clients can call--->>
+    // for this to work, class should have annotation of RestController-->>
+    // it is path provided of a class to get "Hello" msg---->>
+    @GetMapping("/greet")
+    public greetResponse greet() {
+        greetResponse response = new greetResponse("Hello",
+                List.of("Java", "HTML", "CSS", "JavaScript", "Spring"),
+                new Person("John", 30, 40000));
+        return response;
+    }
+
+    record Person(String name, int age, double savings) {
+    }
+
+    record greetResponse(String greet,
+            List<String> favProgrammingLanguages, Person person) {
+    }
+    // this record line is equivalent to greetResponse class---->>
+    // class greetResponse {
+    // private final String greet;
+    // greetResponse(String greet) {
+    // this.greet = greet;
+    // }
+    // public String getGreet() {
+    // return greet;
+    // }
+    // }
 }
