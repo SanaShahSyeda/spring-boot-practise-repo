356f6bd3b78f52a17dce322940f83e5c97a9d727
Syeda Sana <sana.shah1740@gmail.com>
2023-08-20 06:07:47 -0700

    first commit

---
+++ src/main/java/springDemo/Customer.java
@@ -0,0 +1,95 @@
+package springDemo;
+import java.util.Objects;
+
+public class Customer {
+    private Integer id;
+    private Integer age;
+    private String name;
+    private String email;
+
+    public Customer(Integer id, Integer age, String name, String email) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Customer id(Integer id) {
+        setId(id);
+        return this;
+    }
+
+    public Customer age(Integer age) {
+        setAge(age);
+        return this;
+    }
+
+    public Customer name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public Customer email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Customer)) {
+            return false;
+        }
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(age, customer.age) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, age, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", age='" + getAge() + "'" +
+            ", name='" + getName() + "'" +
+            ", email='" + getEmail() + "'" +
+            "}";
+    }
+
+    }
---
+++ src/main/java/springDemo/Main.java
@@ -0,0 +1,54 @@
+package springDemo;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+/*
+ * @Configuration
+ * 
+ * @EnableAutoConfiguration
+ * //telling Spring to look for components in given package.
+ * 
+ * @ComponentScan(basePackages = "springDemo")
+ */
+
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    // expressed as endpoints that clients can call--->>
+    // for this to work, class should have annotation of RestController-->>
+    // it is path provided of a class to get "Hello" msg---->>
+    @GetMapping("/greet")
+    public greetResponse greet() {
+        greetResponse response = new greetResponse("Hello",
+                List.of("Java", "HTML", "CSS", "JavaScript"),
+                new Person("Johnson", 30, 40000));
+        return response;
+    }
+
+    record Person(String name, int age, double savings) {
+    }
+
+    record greetResponse(String greet,
+            List<String> favProgrammingLanguages, Person person) {
+    }
+    // this record line is equivalent to greetResponse class---->>
+    // class greetResponse {
+    // private final String greet;
+    // greetResponse(String greet) {
+    // this.greet = greet;
+    // }
+    // public String getGreet() {
+    // return greet;
+    // }
+    // }
+
+}
