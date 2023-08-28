5ebdc07aedbcb6a8a341a8639ff7a7ea31a64901
Syeda Sana <sanashaaah4@gmail.com>
2023-08-27 22:09:47 -0700

    methods implemented to perform POST request (send data to database that user provides in API)

--- src/main/java/springDemo/CustomerRepository.java
+++ src/main/java/springDemo/CustomerRepository.java
@@ -1,8 +1,13 @@
 package springDemo;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // could perform CRUD operations
 public interface CustomerRepository extends
                 JpaRepository<Customer, Integer> {
+
+        @Override
+        List<Customer> findAll();
+
 }
--- src/main/java/springDemo/Main.java
+++ src/main/java/springDemo/Main.java
@@ -3,6 +3,8 @@ package springDemo;
 import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @RestController
 @RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
@@ -19,20 +27,20 @@ public class Main {
     // for this to work, class should have annotation of RestController-->>
     // it is path provided of a class to get "Hello" msg---->>
 
-    @GetMapping("/greet")
-    public greetResponse greet() {
-        greetResponse response = new greetResponse("Hello",
-                List.of("Java", "HTML", "CSS", "JavaScript", "Spring", "Spring boot"),
-                new Person("John", 30, 40000));
-        return response;
-    }
+    // @GetMapping("/greet")
+    // public greetResponse greet() {
+    // greetResponse response = new greetResponse("Hello",
+    // List.of("Java", "HTML", "CSS", "JavaScript", "Spring", "Spring boot"),
+    // new Person("John", 30, 40000));
+    // return response;
+    // }
 
-    record Person(String name, int age, double savings) {
-    }
+    // record Person(String name, int age, double savings) {
+    // }
 
-    record greetResponse(String greet,
-            List<String> favProgrammingLanguages, Person person) {
-    }
+    // record greetResponse(String greet,
+    // List<String> favProgrammingLanguages, Person person) {
+    // }
     // this record line is equivalent to greetResponse class---->>
     // class greetResponse {
     // private final String greet;
@@ -45,7 +53,22 @@ public class Main {
     // }
 
     @GetMapping
-    public List<Customer> getCustomer() {
-        return List.of();
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age) {
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer(null, null, null, null);
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(null);
     }
 }
